11e8a8a0a4753c815af5efec40a311ae73bd4d3d
rolandas351 <rolandassventickas@gmail.com>
2019-11-29 20:51:26 +0200

    20191129

--- app/script.js
+++ app/script.js
@@ -1,1 +1,64 @@
 'use strict'
+// function myFunction(features) {
+//     var features = document.getElementById("container-pirmas");
+//     features.scrollIntoView();
+//   }
+
+var mySwiper1 = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 120,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: true,
+    },
+    breakpoints: {
+      600: {
+        slidesPerView: 3
+      },
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      dynamicBullets: true,
+      dynamicMainBullets: 3,
+      clickable: true,
+    },
+
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  })
+// 2 lape
+  var mySwiper2 = new Swiper ('.swiper-container-antras', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 800,
+    clickable: true,
+    // autoplay: {
+    //   delay: 1000,
+    //   disableOnInteraction: true,
+    // },
+    breakpoints: {
+      1000: {
+        slidesPerView: 1
+      },
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+ 
+
+  
+
